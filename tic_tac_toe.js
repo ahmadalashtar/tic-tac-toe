6288171dc9579ecf460b16b107492ebc50c30ce4
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-07 01:24:01 +0300

    Get files from Replit.

---
+++ tic_tac_toe.js
@@ -0,0 +1,37 @@
+// Initializes and returns an empty 3x3 Tic Tac Toe board
+function initBoard() {
+  // Your code here
+}
+
+// Displays the current state of the board in the console
+function displayBoard(board) {
+  // Your code here
+}
+
+// Places the player's move on the board if the chosen cell is unoccupied
+// Returns true if the move is valid, false otherwise
+function makeMove(board, row, col, player) {
+  // Your code here
+}
+
+// Checks if the given player has won the game
+// Returns true if the player has won, false otherwise
+function checkForWin(board, player) {
+  // Your code here
+}
+
+// Checks if the game has ended in a draw
+// Returns true if the game is a draw, false otherwise
+function checkForDraw(board) {
+  // Your code here
+}
+
+
+
+module.exports = {
+  initBoard,
+  displayBoard,
+  makeMove,
+  checkForWin,
+  checkForDraw
+};
